63c61b49c302e36a488c56aa0729b3ee7585f3ad
annemarie000 <annemarieboujaoude@gmail.com>
2025-11-13 15:04:46 -0500

    php files

--- backend/Php/room_booking.php
+++ backend/Php/room_booking.php
@@ -1,1 +1,30 @@
 <?php
+
+global $pdo;
+require_once 'database.php';
+
+$data = json_decode(file_get_contents('php://input'), true);
+
+if(!$data){
+    http_response_code(400);
+    exit("No booking received");
+}
+
+$user_id = isset($data['user_id']) ? $data['user_id'] : null;
+$room_id = isset($data['room_id']) ? $data['room_id'] : null;
+$date = isset($data['date']) ? $data['date'] : null;
+$Stime = isset($data['startTime']) ? $data['startTime'] : null;
+$Etime = isset($data['endTime']) ? $data['endTime'] : null;
+
+if (!$user_id || !$room_id || !$date || !$Stime || !$Etime) {
+    http_response_code(400);
+    exit("Missing booking fields.");
+}
+
+$checkUser = $pdo->prepare("SELECT user_id FROM users WHERE user_id = ?");
+$checkUser->execute([$user_id]);
+
+if (!$checkUser->fetch()) {
+    http_response_code(400);
+    exit("User not found.");
+}
